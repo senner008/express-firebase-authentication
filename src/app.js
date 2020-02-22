d66ee6eb23a20706556389de20fb4fc573a3c0f5
nig <nig@beepanalytics.com>
2020-02-22 19:06:45 +0100

    wip

--- src/app.js
+++ src/app.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const app = express();
 const port = 3000
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 
 const admin = require("../admin.js")
